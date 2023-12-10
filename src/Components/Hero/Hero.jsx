097693b1d96ec2fc5ad636f09d77652534781314
Shivanktyagi07 <shivanktyagi976@gmail.com>
2023-12-10 20:49:23 +0530

    setup about section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm SHIVANK TYAGI</h1>
+        <p className={styles.description}>
+        Full MERN Stack-Developer | Transforming ideas into web solutions. Proficient in React, Node.js, and MongoDB. Committed to Innovation and continuous Learning . Let's connect and collaborate!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/image-removebg-preview.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
